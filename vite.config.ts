390e1f1da16bc19198bebf9f61ef75759fe0c120
pouyanparsi <73445069+pouyanparsi@users.noreply.github.com>
2026-01-20 00:51:06 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/appdesigner.group/",
+  base: "./",
   server: {
     host: "::",
     port: 8080,
@@ -20,3 +19,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
